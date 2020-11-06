a115c24ff9f803c02eb04248b314127eb266cd9d
Sahl Khalifa <sahl.abdelrahman@gmail.com>
2020-11-06 07:21:18 +0200

    5: Add typography in header.js

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,14 +1,15 @@
 import React from "react";
+import Typography from "@material-ui/core/Typography";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import useScrollTrigger from "@material-ui/core/useScrollTrigger";
 
-// To set the elevation when scroll down 
+// To set the elevation when scroll down
 function ElevationScroll(props) {
   const { children } = props;
   const trigger = useScrollTrigger({
     disableHysteresis: true,
-    threshold: 0
+    threshold: 0,
   });
 
   return React.cloneElement(children, {
@@ -16,13 +17,13 @@ function ElevationScroll(props) {
   });
 }
 
-
-
 export default function Header() {
   return (
     <ElevationScroll>
       <AppBar position="fixed">
-        <Toolbar>Arc Development</Toolbar>
+        <Toolbar>
+          <Typography variant='h3'>Arc Development</Typography>
+        </Toolbar>
       </AppBar>
     </ElevationScroll>
   );
