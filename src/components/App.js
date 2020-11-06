49d37297479353cdd2620b1cc2123b90b14f5fd9
Sahl Khalifa <sahl.abdelrahman@gmail.com>
2020-11-06 03:09:16 +0200

    4: Setup theme (styling)

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,17 @@
 import React from "react";
+// Add a general theme 
+import { ThemeProvider } from "@material-ui/core/styles";
+import theme from "../components/ui/theme";
+
 import Header from "../components/ui/Header";
 import BackToTop from "../components/ui/BackToTop";
 
 function App(props) {
   return (
-    <div className="App">
+    <ThemeProvider theme={theme}>
       <Header />
       <BackToTop />
-      {[...new Array(170)]
-        .map(
-          () => `Cras mattis consectetur purus sit amet fermentum.
-Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`
-        )
-        .join("\n")}
-      
-    </div>
+    </ThemeProvider>
   );
 }
 
