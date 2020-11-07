15615a23d0e78dc8151e95ee8f302e959db8ef08
Sahl Khalifa <sahl.abdelrahman@gmail.com>
2020-11-07 02:28:20 +0200

    9: Add estimate button

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -3,8 +3,9 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import useScrollTrigger from "@material-ui/core/useScrollTrigger";
 import { makeStyles } from "@material-ui/core/styles";
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Button from "@material-ui/core/Button";
 
 import logo from "../../assets/logo.svg";
 
@@ -22,18 +23,24 @@ function ElevationScroll(props) {
 }
 
 // To set override default styles
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   logo: {
     height: "7em",
   },
   tabContainer: {
-    marginLeft: 'auto',
+    marginLeft: "auto",
   },
   tab: {
     ...theme.typography.tab,
     minWidth: 10,
-    marginLeft: '25px',
-  }
+    marginLeft: "25px",
+  },
+  button: {
+    ...theme.typography.estimate,
+    borderRadius: "15px",
+    margin: "0 25px 0 50px",
+    height: "45px",
+  },
 }));
 
 export default function Header() {
@@ -44,12 +51,19 @@ export default function Header() {
         <Toolbar disableGutters>
           <img src={logo} alt="Company logo" className={classes.logo} />
           <Tabs className={classes.tabContainer}>
-            <Tab className={classes.tab} label='Home'/>
-            <Tab className={classes.tab} label='Services'/>
-            <Tab className={classes.tab} label='The Revolution'/>
-            <Tab className={classes.tab} label='About Us'/>
-            <Tab className={classes.tab} label='Contact Us'/>
+            <Tab className={classes.tab} label="Home" />
+            <Tab className={classes.tab} label="Services" />
+            <Tab className={classes.tab} label="The Revolution" />
+            <Tab className={classes.tab} label="About Us" />
+            <Tab className={classes.tab} label="Contact Us" />
           </Tabs>
+          <Button
+            className={classes.button}
+            variant="contained"
+            color="secondary"
+          >
+            Free Estimate
+          </Button>
         </Toolbar>
       </AppBar>
     </ElevationScroll>
