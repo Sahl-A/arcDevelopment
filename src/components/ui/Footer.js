4d225c6fbbbedfba3fceb88ecc0c8b45f07c3e3b
Sahl Khalifa <sahl.abdelrahman@gmail.com>
2020-11-18 19:08:17 +0200

    28: Footer: responsive grids using hidden

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
-
 import Grid from "@material-ui/core/Grid";
+import Hidden from "@material-ui/core/Hidden";
+
 import footerAdornment from "../../assets/Footer Adornment.svg";
 
 const useStyles = makeStyles((theme) => ({
@@ -24,7 +25,7 @@ const useStyles = makeStyles((theme) => ({
   },
   gridContainer: {
     position: "absolute",
-    margin: "1.5rem 0",
+    margin: "1.5rem 0 1.5rem 15.4rem",
   },
   link: {
     fontSize: ".75rem",
@@ -36,187 +37,186 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function Footer(props) {
-  const {
-    setTabValue,
-    setselectedMenuItemIndex,
-  } = props;
+  const { setTabValue, setselectedMenuItemIndex } = props;
   const classes = useStyles();
   return (
     <footer className={classes.footer}>
-      <Grid
-        container
-        justify="center"
-        className={classes.gridContainer}
-        spacing="10"
-      >
-        <Grid item>
-          <Grid container direction="column" spacing="2">
-            <Grid
-              item
-              className={classes.link}
-              component={Link}
-              onClick={() => {
-                setTabValue(0);
-              }}
-              to="/"
-            >
-              Home
+      <Hidden mdDown>
+        <Grid
+          container
+          justify="center"
+          className={classes.gridContainer}
+          spacing="8"
+        >
+          <Grid item>
+            <Grid container direction="column" spacing="2">
+              <Grid
+                item
+                className={classes.link}
+                component={Link}
+                onClick={() => {
+                  setTabValue(0);
+                }}
+                to="/"
+              >
+                Home
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item>
+            <Grid container direction="column" spacing="2">
+              <Grid
+                item
+                className={classes.link}
+                component={Link}
+                onClick={() => {
+                  setTabValue(1);
+                  setselectedMenuItemIndex(0);
+                }}
+                to="/services"
+              >
+                Services
+              </Grid>
+              <Grid
+                item
+                className={classes.link}
+                component={Link}
+                onClick={() => {
+                  setTabValue(1);
+                  setselectedMenuItemIndex(1);
+                }}
+                to="/customsoftware"
+              >
+                Custom Software Development
+              </Grid>
+              <Grid
+                item
+                className={classes.link}
+                component={Link}
+                onClick={() => {
+                  setTabValue(1);
+                  setselectedMenuItemIndex(2);
+                }}
+                to="/mobileapps"
+              >
+                iOS/Android App Development
+              </Grid>
+              <Grid
+                item
+                className={classes.link}
+                component={Link}
+                onClick={() => {
+                  setTabValue(1);
+                  setselectedMenuItemIndex(3);
+                }}
+                to="/websites"
+              >
+                Website Development
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item>
+            <Grid container direction="column" spacing="2">
+              <Grid
+                item
+                className={classes.link}
+                component={Link}
+                onClick={() => {
+                  setTabValue(2);
+                }}
+                to="/revolution"
+              >
+                The Revolution
+              </Grid>
+              <Grid
+                item
+                className={classes.link}
+                component={Link}
+                onClick={() => {
+                  setTabValue(2);
+                }}
+                to="/revolution"
+              >
+                Vision
+              </Grid>
+              <Grid
+                item
+                className={classes.link}
+                component={Link}
+                onClick={() => {
+                  setTabValue(2);
+                }}
+                to="/revolution"
+              >
+                Technology
+              </Grid>
+              <Grid
+                item
+                className={classes.link}
+                component={Link}
+                onClick={() => {
+                  setTabValue(2);
+                }}
+                to="/revolution"
+              >
+                Process
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item>
+            <Grid container direction="column" spacing="2">
+              <Grid
+                item
+                className={classes.link}
+                component={Link}
+                onClick={() => {
+                  setTabValue(3);
+                }}
+                to="/about"
+              >
+                About Us
+              </Grid>
+              <Grid
+                item
+                className={classes.link}
+                component={Link}
+                onClick={() => {
+                  setTabValue(3);
+                }}
+                to="/about"
+              >
+                History
+              </Grid>
+              <Grid
+                item
+                className={classes.link}
+                component={Link}
+                onClick={() => {
+                  setTabValue(3);
+                }}
+                to="/about"
+              >
+                Team
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item>
+            <Grid container direction="column" spacing="2">
+              <Grid
+                item
+                className={classes.link}
+                component={Link}
+                onClick={() => {
+                  setTabValue(4);
+                }}
+                to="/contact"
+              >
+                Contact Us
+              </Grid>
             </Grid>
           </Grid>
         </Grid>
-        <Grid item>
-          <Grid container direction="column" spacing="2">
-            <Grid
-              item
-              className={classes.link}
-              component={Link}
-              onClick={() => {
-                setTabValue(1);
-                setselectedMenuItemIndex(0);
-              }}
-              to="/services"
-            >
-              Services
-            </Grid>
-            <Grid
-              item
-              className={classes.link}
-              component={Link}
-              onClick={() => {
-                setTabValue(1);
-                setselectedMenuItemIndex(1);
-              }}
-              to="/customsoftware"
-            >
-              Custom Software Development
-            </Grid>
-            <Grid
-              item
-              className={classes.link}
-              component={Link}
-              onClick={() => {
-                setTabValue(1);
-                setselectedMenuItemIndex(2);
-              }}
-              to="/mobileapps"
-            >
-              iOS/Android App Development
-            </Grid>
-            <Grid
-              item
-              className={classes.link}
-              component={Link}
-              onClick={() => {
-                setTabValue(1);
-                setselectedMenuItemIndex(3);
-              }}
-              to="/websites"
-            >
-              Website Development
-            </Grid>
-          </Grid>
-        </Grid>
-        <Grid item>
-          <Grid container direction="column" spacing="2">
-            <Grid
-              item
-              className={classes.link}
-              component={Link}
-              onClick={() => {
-                setTabValue(2);
-              }}
-              to="/revolution"
-            >
-              The Revolution
-            </Grid>
-            <Grid
-              item
-              className={classes.link}
-              component={Link}
-              onClick={() => {
-                setTabValue(2);
-              }}
-              to="/revolution"
-            >
-              Vision
-            </Grid>
-            <Grid
-              item
-              className={classes.link}
-              component={Link}
-              onClick={() => {
-                setTabValue(2);
-              }}
-              to="/revolution"
-            >
-              Technology
-            </Grid>
-            <Grid
-              item
-              className={classes.link}
-              component={Link}
-              onClick={() => {
-                setTabValue(2);
-              }}
-              to="/revolution"
-            >
-              Process
-            </Grid>
-          </Grid>
-        </Grid>
-        <Grid item>
-          <Grid container direction="column" spacing="2">
-            <Grid
-              item
-              className={classes.link}
-              component={Link}
-              onClick={() => {
-                setTabValue(3);
-              }}
-              to="/about"
-            >
-              About Us
-            </Grid>
-            <Grid
-              item
-              className={classes.link}
-              component={Link}
-              onClick={() => {
-                setTabValue(3);
-              }}
-              to="/about"
-            >
-              History
-            </Grid>
-            <Grid
-              item
-              className={classes.link}
-              component={Link}
-              onClick={() => {
-                setTabValue(3);
-              }}
-              to="/about"
-            >
-              Team
-            </Grid>
-          </Grid>
-        </Grid>
-        <Grid item>
-          <Grid container direction="column" spacing="2">
-            <Grid
-              item
-              className={classes.link}
-              component={Link}
-              onClick={() => {
-                setTabValue(4);
-              }}
-              to="/contact"
-            >
-              Contact Us
-            </Grid>
-          </Grid>
-        </Grid>
-      </Grid>
+      </Hidden>
       <img
         alt="footer adorment"
         src={footerAdornment}
